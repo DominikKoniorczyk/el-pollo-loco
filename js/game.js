dfcba0d348700a3dae73130a605cc82c27d11c1b
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2025-12-11 11:22:04 +0100

    Add initial content

---
+++ js/game.js
@@ -0,0 +1,7 @@
+let canvasRev;
+let character = new Image();
+
+function initGame(){
+    canvasRev = document.querySelector("canvas");
+    character.src = "../img/2_character_pepe/1_idle/idle/I-1.png";
+}
