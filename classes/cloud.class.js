7a42c42664678c1ef57a7efd85e9a871d580b380
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2025-12-12 10:18:53 +0100

    add animation for clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,10 +1,18 @@
 class Cloud extends MovableObject {
-    y = 20;
-    width = 500;
-    height = 300;
+    y = 0;
+    width = 720;
+    height = 480;
     
     constructor(){
         super();
         super.loadImage(ImageHub.backgrounds.clouds[0]);
         this.x = Math.random() * 720;
-    }}
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.x -= 0.15;     
+        }, 1000 / 60);
+    }
+}
