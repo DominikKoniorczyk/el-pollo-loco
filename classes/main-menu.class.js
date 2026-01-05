819a236cbe8a1db60bee20dc66f3cb2633770935
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-05 15:06:28 +0100

    add JSDocs to main-menu.class.js

--- classes/main-menu.class.js
+++ classes/main-menu.class.js
@@ -15,15 +15,30 @@ export class MainMenu {
         IntervalHub.startInterval(this.interval60FPS, 1000 / 60);
     }
 
+    /**
+     * Calls the draw method at a 60 FPS interval.
+     */
     interval60FPS = () =>{
         this.draw();
     }
     
+    /**
+     * Clears the canvas and draws the start screen image.
+     */
     draw(){ 
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.addToViewport({src: ImageHub.startScreen, x: 0, y: 0, width: 720, height: 480})
     }
 
+    /**
+     * Draws an image object onto the canvas at the specified position and size.
+     * @param {Object} object - The image data and its canvas placement.
+     * @param {string} object.src - Source URL of the image.
+     * @param {number} object.x - X-coordinate on the canvas.
+     * @param {number} object.y - Y-coordinate on the canvas.
+     * @param {number} object.width - Width of the image.
+     * @param {number} object.height - Height of the image.
+     */
     addToViewport(object){
         const img = new Image();
         img.src = object.src;
