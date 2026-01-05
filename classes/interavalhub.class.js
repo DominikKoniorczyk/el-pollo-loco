4b0637b63fed98453d1dd80e8cae37c3015ed81c
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-05 15:16:03 +0100

    add JSDocs to intervalhub.class.js

--- classes/interavalhub.class.js
+++ classes/interavalhub.class.js
@@ -5,11 +5,19 @@ export class IntervalHub {
     constructor() {
     }   
 
+    /**
+     * Starts a new interval and stores it for later clearing.
+     * @param {Function} callback - The function to execute repeatedly.
+     * @param {number} intervalTime - Time in milliseconds between executions.
+     */
     static startInterval(callback, intervalTime) {
         const newInterval = setInterval(callback, intervalTime);
         this.allIntervals.push(newInterval);
     }
-
+    
+    /**
+     * Clears all stored intervals and resets the interval list.
+     */
     static clearAllIntervals() {
         this.allIntervals.forEach(interval => clearInterval(interval));
         this.allIntervals = [];
