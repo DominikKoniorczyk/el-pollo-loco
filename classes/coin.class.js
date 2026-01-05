3b3daf75ebf8206c867d339acfa65c33d4b6eec0
Dominik Koniorczyk <d.koniorczyk@limesgames.de>
2026-01-05 15:12:20 +0100

    add JSDocs to coin.class.js

--- classes/coin.class.js
+++ classes/coin.class.js
@@ -15,19 +15,34 @@ export class Coin extends CollectableObject {
         this.setWorldPosition(world_tiles);
     }
 
+    /**
+     * Plays a coin animation every 250 intervals of a 10ms timer.
+     * @param {number} globalIntervalCounter - Current count of the global interval.
+     */
     interval10ms(globalIntervalCounter){
         if(globalIntervalCounter % 250 === 0) this.playAnimation(ImageHub.coin);
     }
 
+    /**
+     * Calls the parent class's 60 FPS update routine.
+     */
     interval60FPS(){
         super.interval60FPS();
     }
 
+    /**
+     * Sets the object's world position to a random x and y coordinate.
+     * @param {Array} world_tiles - Array representing the world tiles for x positioning.
+     */
     setWorldPosition(world_tiles){
         this.y = this.getRandomHeight();  
         this.x = this.getRandomX(world_tiles);
     }
 
+    /**
+     * Returns a random height within the object's min and max height bounds.
+     * @returns {number} Random height value.
+     */
     getRandomHeight(){
         const difference = this.maxHeight - this.minHeight;
         let random = Math.random();
